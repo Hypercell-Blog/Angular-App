4f7cbff14ff1d962ec07f4562b3a31f85bc285f2
Gehad Ahmad <gehad0ahmed28@gmail.com>
2023-09-12 10:31:48 +0300

    Slider Edit

--- src/app/Components/home/carousel/carousel.component.ts
+++ src/app/Components/home/carousel/carousel.component.ts
@@ -1,5 +1,9 @@
 import { Component, Input } from '@angular/core';
 
+export interface SlideInterface {
+  url: string;
+  title: string;
+}
 
 @Component({
   selector: 'app-carousel',
@@ -8,32 +12,14 @@ import { Component, Input } from '@angular/core';
 })
 
 export class CarouselComponent {
-  slides = [
-    {
-      headline: "For Your Current Mood",
-      src:
-        "https://images.unsplash.com/photo-1567653418876-5bb0e566e1c2?ixlib=rb-1.2.1&auto=format&fit=crop&w=2000&q=80"
-    },
-    {
-      headline: "Miouw",
-      src:
-        "https://images.unsplash.com/photo-1559181567-c3190ca9959b?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=2000&q=80"
-    },
-    {
-      headline: "In The Wilderness",
-      src:
-        "https://images.unsplash.com/photo-1557800634-7bf3c7305596?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=2001&q=80"
-    },
-    {
-      headline: "Focus On The Writing",
-      src:
-        "https://images.unsplash.com/photo-1551410224-699683e15636?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=2000&q=80"
-    }
-  ]
+  @Input() slides: SlideInterface[] = [];
+
+  currentIndex: number = 0;
 
   currentSlide = 0;
 
-  constructor() {}
+  constructor() {
+  }
 
   onPreviousClick() {
     const previous = this.currentSlide - 1;
@@ -45,5 +31,13 @@ export class CarouselComponent {
     const next = this.currentSlide + 1;
     this.currentSlide = next === this.slides.length ? 0 : next;
     console.log("next clicked, new current slide is: ", this.currentSlide);
+  }
+
+  goToSlide(index: number){
+    this.currentIndex = index;
+  }
+
+  getUrl(){
+    return this.slides[this.currentIndex].url;
   }
 }
--- src/app/Components/home/home.component.ts
+++ src/app/Components/home/home.component.ts
@@ -6,4 +6,23 @@ import { Component } from '@angular/core';
   styleUrls: ['./home.component.css']
 })
 export class HomeComponent {
+
+  slides = [
+    {
+      title: "For Your Current Mood",
+      url:
+        "https://images.unsplash.com/photo-1567653418876-5bb0e566e1c2?ixlib=rb-1.2.1&auto=format&fit=crop&w=2000&q=80"
+    },
+    {
+      title: "Miouw",
+      url:
+        "https://images.unsplash.com/photo-1559181567-c3190ca9959b?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=2000&q=80"
+    },
+    {
+      title: "In The Wilderness",
+      url:
+        "https://images.unsplash.com/photo-1551410224-699683e15636?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=2000&q=80"
+    }
+  ]
+
 }
