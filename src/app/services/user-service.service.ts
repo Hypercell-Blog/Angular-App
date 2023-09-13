679c21868ed7b66332b2fb6adad4270bb6794271
Gehad Ahmad <gehad0ahmed28@gmail.com>
2023-09-13 17:06:35 +0300

    userService

--- src/app/services/user-service.service.ts
+++ src/app/services/user-service.service.ts
@@ -5,16 +5,37 @@ import { Observable, map } from 'rxjs';
 @Injectable({
     providedIn: 'root'
 })
+
 export class UserService {
 
+    response = {
+        Confirmpassword: "123456789",
+        email: "gehad0ahmed28@gmail.com",
+        id: 101,
+        name:  "Gehad",
+        password: "123456789"
+    };
+
     constructor(
-        private _user: HttpClient
+        private _http: HttpClient
     ) { }
 
-    registerUser(userData: any): Observable<any> {
-        return this._user.post('', userData);
+    registerUser(userData: any): any {
+        return this._http.post('https://jsonplaceholder.typicode.com/posts', userData);
     }
 
+    login(userData: any): any {
+        return this._http.post('https://jsonplaceholder.typicode.com/posts', userData).subscribe({
+           next: (response: any) => localStorage.setItem('id', response.id)
+        });
+    }
 
-
+    getUser(): any{
+        let userId = localStorage.getItem('id');
+        return this._http.get("https://jsonplaceholder.typicode.com/posts/" + userId).subscribe({
+            next: (user) => {
+                return user;
+            } 
+        })
+    }
 }
