2786cf5d50e78a7a74eaa9d986c75a07eb3d4a12
hebasherif1 <hebasherif40@gmail.com>
2023-09-13 14:06:50 +0300

    Navbar Edit

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -13,6 +13,7 @@ import { PostDetailsComponent } from './Components/post-details/post-details.com
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import {MatButtonModule} from '@angular/material/button';
 import {MatIconModule} from '@angular/material/icon';
+import {MatGridListModule} from '@angular/material/grid-list';
 
 @NgModule({
   declarations: [
@@ -31,7 +32,8 @@ import {MatIconModule} from '@angular/material/icon';
     BrowserAnimationsModule,
     MatButtonModule,
     MatIconModule,
-  
+    MatGridListModule,
+
   ],
   providers: [],
   bootstrap: [AppComponent]
