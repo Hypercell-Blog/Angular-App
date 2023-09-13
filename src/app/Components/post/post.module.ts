e103dd60648612b7787a4e08936742cfc4e43e28
Raneen Gaber <78453166+RaneenGaber@users.noreply.github.com>
2023-09-13 17:14:03 +0300

    import modul

--- src/app/Components/post/post.module.ts
+++ src/app/Components/post/post.module.ts
@@ -7,6 +7,7 @@ import { MatPaginatorModule } from "@angular/material/paginator";
 import { MatButtonModule } from "@angular/material/button";
 import {MatCardModule} from '@angular/material/card';
 import { PostRoutingModule } from "./post-routing.module";
+import { MatIconModule } from "@angular/material/icon";
 
 @NgModule({
     declarations: [
@@ -19,7 +20,8 @@ import { PostRoutingModule } from "./post-routing.module";
         MatPaginatorModule,
         MatButtonModule,
         MatCardModule,
-        PostRoutingModule
+        PostRoutingModule,
+        MatIconModule
     ],
     exports: [
         PostDetailsComponent,
