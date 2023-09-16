249bfbc2aa92f465ddcc7045709d0ff9c9e211e6
yasmeenahmedh <144279606+yasmeenahmedh@users.noreply.github.com>
2023-09-16 15:32:48 +0300

    confirmpasswordvali

--- src/app/Components/register/register.component.ts
+++ src/app/Components/register/register.component.ts
@@ -1,6 +1,7 @@
 import { Component } from '@angular/core';
-import { FormBuilder, FormGroup, Validators } from '@angular/forms';
+import { FormBuilder, FormGroup, Validators, } from '@angular/forms';
 import { UserService } from 'src/app/services/user-service.service';
+
 @Component({
   selector: 'app-register',
   templateUrl: './register.component.html',
@@ -12,16 +13,31 @@ export class RegisterComponent {
     private _fb: FormBuilder,
     private userService: UserService
   ) {
-    this.registerform = this._fb.group({
-      name: [null, [Validators.required]],
-      email: [null, [Validators.required]],
-      password: [null, [Validators.minLength(6)]],
-      Confirmpassword: [null, [Validators.required]]
-
-    })
+    this.registerform = this._fb.group(
+      {
+        name: [null, [Validators.required]],
+        email: [null, [Validators.required]],
+        password: [null, [Validators.minLength(6)]],
+        Confirmpassword: [null, [Validators.required]]
+      },
+      {
+        validator: this.confirmPasswordMatch('password', 'Confirmpassword')
+      }
+    )
   }
   getError(control: string, error: string): boolean {
     return this.registerform.controls[control].touched && this.registerform.controls[control].hasError(error);
+  }
+  confirmPasswordMatch(controlName: string, matchingControlName: string) {
+    return (formGroup: FormGroup) => {
+      const control = formGroup.controls[controlName];
+      const matchingControl = formGroup.controls[matchingControlName];
+      if (control.value !== matchingControl.value) {
+        matchingControl.setErrors({ confirmPasswordMatch: true });
+      } else {
+        matchingControl.setErrors(null);
+      }
+    }
   }
 
   submitForm() {
